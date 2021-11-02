e3bd4ac0dab34624279eeed67e0f9da4623c3c8c
Korey Lombardi <koreylombardi17@yahoo.com>
2021-11-02 14:46:47 -0400

    Implemented 2d arr of ints. Need to write code to populate bbd.

--- flowComputing.c
+++ flowComputing.c
@@ -9,22 +9,25 @@ struct Node {
 	Node* left_child, *right_child;
 };
 
-void print_input_file(char*);
+void print_input_file(char*); // Maybe delete this function later
 Node* create_node(int, Node*, Node*, int);
-char** file_to_array(char*, int, int);
+char** file_to_strings_arr(char*, int, int);
+int** strings_arr_to_ints_arr(char**, int, int);
 int get_num_vars(char*);
 int get_num_nodes(char*);
-char* test_function();
-void free_input_data_arr(char**, int);
+void free_strings_arr(char**, int);
+void free_ints_arr(int**, int);
 
 int main() {
 	char* path = "input_files/bdds/var25.bdd";
 	int num_vars = get_num_vars(path);
 	int num_nodes = get_num_nodes(path);
-	char** input_data_arr = file_to_array(path, num_vars, num_nodes);
-	
+	char** data_strings_arr = file_to_strings_arr(path, num_vars, num_nodes);
+	int** data_ints_arr = strings_arr_to_ints_arr(data_strings_arr, num_vars, num_nodes);
 
-	free_input_data_arr(input_data_arr, num_vars);	
+	// Free memory
+	free_strings_arr(data_strings_arr, num_vars);	
+	free_ints_arr(data_ints_arr, num_vars);
 	return 0;
 
 }
@@ -59,7 +62,7 @@ Node* create_node(int node_id, Node* left_child, Node* right_child, int decision
 }
  
 // Function converts input file into an array of strings
-char** file_to_array(char* path, int num_vars, int num_nodes) {
+char** file_to_strings_arr(char* path, int num_vars, int num_nodes) {
 	char** input_data_arr = calloc(num_vars, sizeof(char*));
 	FILE* fptr;
 	size_t length = 0; // Buffer's length in bytes
@@ -86,6 +89,39 @@ char** file_to_array(char* path, int num_vars, int num_nodes) {
 	return input_data_arr;
 }
 
+// Function converts the arr of strings into a 2d array of ints
+int** strings_arr_to_ints_arr(char** data_strings_arr, int num_vars, int num_nodes) {
+	int** ints_arr = calloc(num_vars, sizeof(int*));
+	for(int i = 0; i < num_vars; i++) {
+		ints_arr[i] = calloc(4, sizeof(int)); // 4 int's per line of data
+	}
+	
+	char temp[3]; // Temp string that will store each value 
+	memset(temp, '\0', 3); // Initialize temp string
+	int temp_index = 0; // Index used for temp string
+	int int_index = 0; // Index to keep track of which int is being worked on 
+	// Loop through each variable
+	for(int i = 0; i < num_vars; i++) {
+		char* original_ptr_location = data_strings_arr[i]; // Used to psoition pointer to its original location
+		// Loop to the end of line
+		while(*data_strings_arr[i] != '\n') { 
+			// Loop until a space is reached
+			while((*data_strings_arr[i] != ' ') && (*data_strings_arr[i] != '\n')) { 
+				temp[temp_index++] = *data_strings_arr[i]++;
+			}
+			ints_arr[i][int_index++] = atoi(temp);
+			temp_index = 0; // Reset temp index in order to reset temp string
+			memset(temp, '\0', 3); // Reinitialize temp string
+			if(*data_strings_arr[i] != '\n') {
+				data_strings_arr[i]++; // Move from ' ' to next char
+			}
+		}
+		data_strings_arr[i] = original_ptr_location;
+		int_index = 0; // Reset int index since moving to the next line
+	}
+	return ints_arr;
+}
+
 // Function returns the number of vars 
 int get_num_vars(char* path) {
 	FILE* fptr;
@@ -101,7 +137,7 @@ int get_num_vars(char* path) {
 
 	// Copying input from first line, starting at the 5th element
 	nread = getline(&buffer, &length, fptr);
-	buffer+=5;
+	buffer+=5; // Position buffer's ptr to where the index of the num of vars exists
 	
 	int counter = 0;
 	while(*buffer != '\n') {
@@ -131,7 +167,7 @@ int get_num_nodes(char* path) {
 	// Copying input from second line, starting at the 6th element
 	nread = getline(&buffer, &length, fptr);
 	nread = getline(&buffer, &length, fptr);
-	buffer+=6;
+	buffer+=6; // Position buffer's ptr to where the index of the num of nodes exists
 	
 	int counter = 0;
 	while(*buffer != '\n') {
@@ -145,10 +181,18 @@ int get_num_nodes(char* path) {
 	return atoi(num_nodes_str);
 }
 
-// Funtions frees the input_data_arr
-void free_input_data_arr(char** input_data_arr, int num_vars) {
+// Funtion frees the input_data_arr
+void free_strings_arr(char** input_data_arr, int num_vars) {
 	for(int i = 0; i < num_vars; i++) {
 		free(input_data_arr[i]);
 	}
 	free(input_data_arr);
+}
+
+// Funtion frees the input_ints_arr
+void free_ints_arr(int** ints_arr, int num_vars) {
+	for(int i = 0; i < num_vars; i++) {
+		free(ints_arr[i]);
+	}
+	free(ints_arr);
 }
