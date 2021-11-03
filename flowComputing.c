f401009bfb0a461e01c6e6c80842a722439d8c40
ko320078 <ko320078@eustis2>
2021-11-02 20:25:51 -0400

    Created BDD. Need to free the memory for each node.

--- flowComputing.c
+++ flowComputing.c
@@ -9,27 +9,31 @@ struct Node {
 	Node* left_child, *right_child;
 };
 
-void print_input_file(char*); // Maybe delete this function later
+void print_input_file(char*); // TODO: Maybe delete this function later
+void print_strings_arr(char**, int); // TODO: Delete later
+void print_ints_arr(int**, int); // TODO: Delete later
 Node* create_node(int, Node*, Node*, int);
 char** file_to_strings_arr(char*, int, int);
 int** strings_arr_to_ints_arr(char**, int, int);
 int get_num_vars(char*);
 int get_num_nodes(char*);
+Node* create_bdd(int**, int);
 void free_strings_arr(char**, int);
 void free_ints_arr(int**, int);
 
 int main() {
-	char* path = "input_files/bdds/var25.bdd";
+	char* path = "input_files/bdds/var2.bdd";
 	int num_vars = get_num_vars(path);
 	int num_nodes = get_num_nodes(path);
 	char** data_strings_arr = file_to_strings_arr(path, num_vars, num_nodes);
 	int** data_ints_arr = strings_arr_to_ints_arr(data_strings_arr, num_vars, num_nodes);
+	
+	Node* head = create_bdd(data_ints_arr, num_nodes);
 
 	// Free memory
-	free_strings_arr(data_strings_arr, num_vars);	
-	free_ints_arr(data_ints_arr, num_vars);
+	free_strings_arr(data_strings_arr, num_nodes);	
+	free_ints_arr(data_ints_arr, num_nodes);
 	return 0;
-
 }
 
 // Function that reads and prints the input file
@@ -50,6 +54,23 @@ void print_input_file(char* path) {
 	free(buffer);
 }
 
+void print_strings_arr(char** strings_arr, int num_nodes) {
+	printf("Printing as string array.\n");	
+	for(int i = 0; i < num_nodes; i++) {
+		printf("%s", strings_arr[i]);
+	}
+}
+
+void print_ints_arr(int** ints_arr, int num_nodes) {
+	printf("Printing as int array.\n");	
+	for(int i = 0; i < num_nodes; i++) {
+		for(int j = 0; j < 4; j++){
+			printf("%d ", ints_arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+
 
 // Function creates a new node
 Node* create_node(int node_id, Node* left_child, Node* right_child, int decision_id) {
@@ -63,7 +84,7 @@ Node* create_node(int node_id, Node* left_child, Node* right_child, int decision
  
 // Function converts input file into an array of strings
 char** file_to_strings_arr(char* path, int num_vars, int num_nodes) {
-	char** input_data_arr = calloc(num_vars, sizeof(char*));
+	char** input_data_arr = calloc(num_nodes, sizeof(char*));
 	FILE* fptr;
 	size_t length = 0; // Buffer's length in bytes
 	ssize_t nread;// Variable stores the number of chars read into line
@@ -79,7 +100,7 @@ char** file_to_strings_arr(char* path, int num_vars, int num_nodes) {
 	nread = getline(&buffer, &length, fptr);
 	
 	// Loop through each line, storing it's data
-	for (int i = 0; i < num_vars; i++) {
+	for (int i = 0; i < num_nodes; i++) {
 		nread = getline(&buffer, &length, fptr);
 		input_data_arr[i] = calloc(nread+1, sizeof(char));
 		strcpy(input_data_arr[i], buffer);
@@ -91,8 +112,8 @@ char** file_to_strings_arr(char* path, int num_vars, int num_nodes) {
 
 // Function converts the arr of strings into a 2d array of ints
 int** strings_arr_to_ints_arr(char** data_strings_arr, int num_vars, int num_nodes) {
-	int** ints_arr = calloc(num_vars, sizeof(int*));
-	for(int i = 0; i < num_vars; i++) {
+	int** ints_arr = calloc(num_nodes, sizeof(int*));
+	for(int i = 0; i < num_nodes; i++) {
 		ints_arr[i] = calloc(4, sizeof(int)); // 4 int's per line of data
 	}
 	
@@ -101,7 +122,7 @@ int** strings_arr_to_ints_arr(char** data_strings_arr, int num_vars, int num_nod
 	int temp_index = 0; // Index used for temp string
 	int int_index = 0; // Index to keep track of which int is being worked on 
 	// Loop through each variable
-	for(int i = 0; i < num_vars; i++) {
+	for(int i = 0; i < num_nodes; i++) {
 		char* original_ptr_location = data_strings_arr[i]; // Used to psoition pointer to its original location
 		// Loop to the end of line
 		while(*data_strings_arr[i] != '\n') { 
@@ -179,6 +200,26 @@ int get_num_nodes(char* path) {
 	buffer-=(counter+6);
 	free(buffer);
 	return atoi(num_nodes_str);
+}
+
+Node* create_bdd(int** ints_arr, int num_nodes) {
+	Node* nodes_arr[num_nodes];
+	int left_child_index = 0;
+	int right_child_index = 0;
+	
+	for(int i = 0; i < num_nodes; i++) {
+		nodes_arr[i] = create_node(ints_arr[i][0], NULL, NULL, ints_arr[i][3]);
+	}
+
+	for(int i = 0; i < num_nodes; i++) {
+		left_child_index = ints_arr[i][1] - 1;
+		right_child_index = ints_arr[i][2] - 1;
+		if(!(left_child_index < 0)){
+			nodes_arr[i]->left_child = nodes_arr[left_child_index];
+			nodes_arr[i]->right_child = nodes_arr[right_child_index];
+		}
+	}
+	return nodes_arr[0];
 }
 
 // Funtion frees the input_data_arr
